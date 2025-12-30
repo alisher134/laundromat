1fddef2df0a06c6e087c8fdbeb2579e5c3eda6fe
alisher134 <alisherrakhmanov134@gmail.com>
2025-12-30 14:16:39 +0500

    fix: added responsive positioning of map elements for various screen sizes

--- src/widgets/LocationSection/LocationSectionMap.tsx
+++ src/widgets/LocationSection/LocationSectionMap.tsx
@@ -23,7 +23,7 @@ interface LocationSectionMapProps {
 export const LocationSectionMap = ({ activeLocationId, onLocationSelect }: LocationSectionMapProps) => {
   return (
     <div className="z-background absolute top-[200px] right-0 left-0 h-[698px] w-[809px] md:top-[150px] md:h-[751px] md:w-[1018px] xl:top-0 xl:h-[952px] xl:w-[1104px] 2xl:h-[1337px] 2xl:w-[1551px]">
-      <div className="absolute h-full w-full -translate-x-[260px] -translate-y-[20px] min-[360px]:-translate-x-[240px] min-[360px]:-translate-y-[60px] min-[375px]:-translate-x-[220px] min-[375px]:-translate-y-[55px] min-[390px]:-translate-x-[200px] min-[390px]:-translate-y-[50px] min-[425px]:-translate-x-[178px] min-[425px]:-translate-y-[40px] md:hidden">
+      <div className="absolute h-full w-full -translate-x-[260px] -translate-y-[20px] min-[360px]:-translate-x-[240px] min-[360px]:-translate-y-[60px] min-[375px]:-translate-x-[220px] min-[375px]:-translate-y-[55px] min-[390px]:-translate-x-[200px] min-[390px]:-translate-y-[50px] min-[425px]:-translate-x-[178px] min-[425px]:-translate-y-[40px] sm:translate-x-[100px] md:hidden">
         <Image
           alt="Map showing laundromat locations in the area"
           className="object-contain"
@@ -43,7 +43,7 @@ export const LocationSectionMap = ({ activeLocationId, onLocationSelect }: Locat
         ))}
       </div>
 
-      <div className="absolute hidden h-full w-full -translate-x-[50px] md:block xl:translate-x-[503px] xl:translate-y-[-50px] 2xl:translate-x-[800px]">
+      <div className="absolute hidden h-full w-full -translate-x-[50px] md:block md:translate-x-[53px] md:translate-y-[-100px] lg:translate-x-[183px] lg:translate-y-[-100px] xl:translate-x-[503px] xl:translate-y-[-50px] 2xl:translate-x-[800px]">
         <Image alt="" aria-hidden="true" className="object-contain" fill priority src="/images/desktop-map-preview.png" />
         {DESKTOP_LOCATIONS.map((position) => (
           <div
